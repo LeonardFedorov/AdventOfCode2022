cf63b3dfe37a9156ca41b266627b6ad43ced6133
LeonardFedorov <oliver.ingamells@gmail.com>
2021-12-10 00:16:28 +0000

    Refactor of Day 9 part 1 to make part 2 easier

--- Days 1 to 10/Day9.fs
+++ Days 1 to 10/Day9.fs
@@ -7,30 +7,33 @@ open System.IO
 //Common
 let getText projectDir =
     let fileStream = new StreamReader(projectDir + "\\Day9Input.txt")
-    let stringArray = fileStream.ReadToEnd().Split([|"\r\n"|], StringSplitOptions.None)
-    Array2D.init stringArray.[0].Length stringArray.Length (fun i j -> int stringArray.[i].[j] - int '0')
+    fileStream.ReadToEnd().Split([|"\r\n"|], StringSplitOptions.None)
+    |> Array.map (fun stringRow -> Array.map (fun c -> int c - int '0') (stringRow.ToCharArray()) )
  
  //Part 1
 
-let testPoint i j (sourceArray: int [,]) point =
-    if i >= 0 && i < sourceArray.GetLength(0) &&
-       j >= 0 && j < sourceArray.GetLength(1) then
-        sourceArray.[i,j] > point
+let testPoint i j (sourceArray: int [][]) point =
+    if i >= 0 && i < sourceArray.Length &&
+       j >= 0 && j < sourceArray.[i].Length then
+        sourceArray.[i].[j] > point
     else
         true
     
-let isPit i j (sourceArray: int [,]) =
-    let element = sourceArray.[i,j]
+let isPit i j (sourceArray: int[][]) =
+    let element = sourceArray.[i].[j]
 
     testPoint (i-1) j sourceArray element &&
     testPoint (i+1) j sourceArray element &&
     testPoint i (j-1) sourceArray element &&
     testPoint i (j+1) sourceArray element
 
-let part1Compute sourceArray =   
-    Array2D.mapi(fun i j x -> if isPit i j sourceArray then x + 1 else 0) sourceArray
-    |> Seq.cast<int>
-    |> Seq.sum 
+let getLowsfromRow sourceRow lowList sourceArray i =
+    Array.fold (fun (list, j) x -> if (isPit i j sourceArray) then ((i,j) :: list, j+1) else (list, j+1)) (lowList, 0) sourceRow
+    |> fst
+
+let getLowList sourceArray =   
+    Array.fold (fun (list, i) x -> ((getLowsfromRow x list sourceArray i), i+1) ) (List.empty<(int * int)> , 0) sourceArray
+    |> fst
 
  //Part 2
 
@@ -40,7 +43,11 @@ let part1Compute sourceArray =
 let main projectDir =
 
     let sourceData = getText projectDir
+    let lowsList = getLowList sourceData
+    let riskSum = List.fold(fun s (i,j) -> s + 1 + sourceData.[i].[j]) 0 lowsList
 
-    Console.WriteLine("Part 1: " + (part1Compute sourceData).ToString()  )
+
+
+    Console.WriteLine("Part 1: " + riskSum.ToString() )
     Console.WriteLine("Part 2: Not Implemented."  )
     9
