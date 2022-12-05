516fbb97d481c286a92c4ec8c6feac227856b651
Oliver Ingamells <oliver.ingamells@gmail.com>
2022-12-05 12:30:51 +0000

    Initial hacky solution to day 5

--- Days 1 to 9/Day5.fs
+++ Days 1 to 9/Day5.fs
@@ -7,26 +7,51 @@ open System.IO
 //Entry point
 let main projectDir =
 
-    //Common
+    //Split the text stream by the double line break to separate the move list from the config
     let sourceData =
         let fileStream = new StreamReader(projectDir + "\\Day5Input.txt")
-        fileStream.ReadToEnd().Split([|"\r\n"|], StringSplitOptions.None)
+        fileStream.ReadToEnd().Split([|"\r\n\r\n"|], StringSplitOptions.None)
+        |> Array.map (fun section -> section.Split([|"\r\n"|], StringSplitOptions.None))
 
+    //Get the list of moves
+    let moveList =
+        Array.map (fun (line: string) -> line.Split([|"move ";" from ";" to "|], StringSplitOptions.RemoveEmptyEntries)) sourceData.[1]
+        |> Array.map (fun valueList -> Array.map (fun value -> Int32.Parse(value)) valueList) 
+        
+    //Build the initial box state
+    let initialBoxList = 
+        let stackCount = 9
+        let mutable boxBuild = Array.init stackCount (fun i -> List.empty<char>)
+        //Set up the initial state of the boxList
+        for i in sourceData.[0].Length - 2 .. -1 .. 0 do
+            for j in 1 .. stackCount do
+                match sourceData.[0].[i].[4*j - 3] with
+                    | ' ' -> ()
+                    | c -> boxBuild.[j-1] <- c :: boxBuild.[j-1]
+        boxBuild
 
-    //Part 1
+    let rec moveBoxes (boxList: char list []) count blockSize source destination =
+        if count = 0 then ()
+        else
+            boxList.[destination] <- List.take blockSize boxList.[source] @ boxList.[destination]
+            boxList.[source] <- List.skip blockSize boxList.[source]
+            moveBoxes boxList (count - blockSize) blockSize source destination
 
+    let readTopBoxes (boxList: char list []) =
+        Array.init (boxList.Length) (fun i -> boxList.[i].Head)
+        |> String
 
+    let part1 =
+        let mutable boxState = initialBoxList
+        Array.iter (fun [|count; source; destination|] -> moveBoxes boxState count 1 (source-1) (destination-1)) moveList
+        readTopBoxes boxState
+        "lol"
 
+    let part2 =
+        let mutable boxState = initialBoxList
+        Array.iter (fun [|count; source; destination|] -> moveBoxes boxState count count (source-1) (destination-1)) moveList
+        readTopBoxes boxState
 
-
-
-
-    //Part 2
-
-
-
-    //Output
-
-    Console.WriteLine("Part 1: Not Implemented."  )
-    Console.WriteLine("Part 2: Not Implemented."  )
+    Console.WriteLine("Part 1: " + part1)
+    Console.WriteLine("Part 2: " + part2)
     5
