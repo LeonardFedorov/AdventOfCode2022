a9d6d00bb4455e33cc5ef55edd48bc2d06fe9e1c
Oliver Ingamells <oliver.ingamells@gmail.com>
2022-12-10 11:33:35 +0000

    Day 9 part 1

--- Days 1 to 9/Day9.fs
+++ Days 1 to 9/Day9.fs
@@ -11,22 +11,62 @@ let main projectDir =
     let sourceData =
         let fileStream = new StreamReader(projectDir + "\\Day9Input.txt")
         fileStream.ReadToEnd().Split([|"\r\n"|], StringSplitOptions.None)
-
+        |> Array.map (fun line -> (line.[0] , Int32.Parse(line.[2..])))
+        |> List.ofArray
 
     //Part 1
 
+    let findTailPath (ropeLength: int) =
+        
+        let customAbs (value: int) = Math.Abs(value)
+        let customSign (value: int) = Math.Sign(value)
 
+        let rec processInstruction (hx, hy) (tx, ty) pathSoFar direction count =
+            
+            if count = 0 then
+                ((hx, hy), (tx, ty), pathSoFar)
+            else
 
+                let (hx2, hy2) = match direction with
+                                    | 'U' -> (hx, hy + 1)
+                                    | 'D' -> (hx, hy - 1)
+                                    | 'L' -> (hx - 1, hy)
+                                    | 'R' -> (hx + 1, hy)
+                                    | _ -> failwith "Unexpected direction"
 
+                let moveTail = (customAbs(hx2 - tx) > ropeLength) || (customAbs(hy2 - ty) > ropeLength)
 
+                let (txd, tyd) = if not moveTail then (0,0)
+                                 else 
+                                     (customSign(hx2 - tx), customSign(hy2 - ty))
 
+                let newTail = (tx + txd , ty + tyd)                            
+                let newPath = if moveTail then
+                                  newTail :: pathSoFar
+                              else pathSoFar
 
-    //Part 2
+                processInstruction (hx2, hy2) newTail newPath direction (count - 1)
+        
+        let rec processPathIter instructions pathSoFar headPos tailPos =
+            
+            if List.isEmpty instructions then
+                pathSoFar
+            else
+                let (direction, count) = List.head instructions
+                let (newHead, newTail, newPath) = processInstruction headPos tailPos pathSoFar direction count
+                processPathIter (List.tail instructions) newPath newHead newTail
 
-
+        processPathIter sourceData [(0,0)] (0,0) (0,0)
 
     //Output
+    let p1Result = findTailPath 2
+                   |> List.distinct
+                   |> List.length
 
-    Console.WriteLine("Part 1: Not Implemented."  )
-    Console.WriteLine("Part 2: Not Implemented."  )
+    let p2Result = findTailPath 9
+                   |> List.distinct
+                   |> List.length
+
+    Console.WriteLine("Part 1: " + p1Result.ToString())
+    Console.WriteLine("Part 2: " + p2Result.ToString())
     9
